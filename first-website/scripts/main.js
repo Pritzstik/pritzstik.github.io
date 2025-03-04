a884a96ae7f2cb5b7505ce41bac348e6142fd103
Pritzstik <pritam.vekaria@gmail.com>
2025-03-04 09:11:33 +0000

    commit

---
+++ first-website/scripts/main.js
@@ -0,0 +1,20 @@
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Welcome to my first webpage ${myName}!`;
+  }
+  
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Welcome to my first webpage ${storedName}!`;
+}
+
+myButton.addEventListener("click", () => {
+    setUserName();
+});
